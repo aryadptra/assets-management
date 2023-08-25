fbd6cd8597dfe9a58d3d12339efe46eafddadf7d
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-08-25 17:00:14 +0700

    Create users seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
